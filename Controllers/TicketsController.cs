3f4ee293c5b76f459b79335c8702400a62d47e85
lacr2591 <66636288+lacr2591@users.noreply.github.com>
2022-08-27 17:12:19 -0500

    Create TicketsController.cs
    TicketController

---
+++ Controllers/TicketsController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bbva_backend.Models;
+
+namespace bbva_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+
+        starWebDbContext db = new starWebDbContext();
+
+
+        [HttpPost]
+        [Route("CrearTicket")]
+        public IActionResult CrearTicket(string dni, int idAgencia)
+        {
+            int idCliente = db.Clientes.Where(x => x.NumeroDocumento == dni).Select(x => x.IdCliente).FirstOrDefault();
+
+            db.Tickets.Add(new Ticket
+            {
+                IdTicket = 0,
+                Estado = "C",
+                IdAgencia = idAgencia,
+                IdCliente = idCliente == 0 ? null : idCliente,
+                FechaHoraIngreso = DateTime.Now.ToString(),
+                AtendidoPor = ""
+
+            });
+            db.SaveChanges();
+
+            string mensaje = "";
+
+            return Ok(mensaje);
+        }
+
+        [HttpPost]
+        [Route("AtenderTicket")]
+        public IActionResult AtenderTicket(int idTicket)
+        {
+
+            var objTicket = db.Tickets.Where(x => x.IdTicket == idTicket).FirstOrDefault();
+            if (objTicket != null)
+            {
+                objTicket.Estado = "A";
+                db.SaveChanges();
+            }
+            string mensaje = "";
+
+            return Ok(mensaje);
+        }
+
+        [HttpPost]
+        [Route("TerminarTicket")]
+        public IActionResult TerminarTicket(int idTicket)
+        {
+            var objTicket = db.Tickets.Where(x => x.IdTicket == idTicket).FirstOrDefault();
+            if (objTicket != null)
+            {
+                objTicket.Estado = "T";
+                db.SaveChanges();
+            }
+            string mensaje = "";
+
+            return Ok(mensaje);
+        }
+
+        [HttpGet]
+        [Route("ObtenerTicketsAtendiendose")]
+        public IActionResult ObtenerTicketsAtendiendose(int idAgencia)
+        {
+            string fechaHoy = DateTime.Now.ToString("dd/MM/yyyy");
+            var objTickets = db.Tickets.Where(x => x.IdAgencia == idAgencia && x.Estado=="A"&& x.FechaHoraIngreso.Substring(0,10)==fechaHoy ).ToList() ;
+
+
+            return Ok(objTickets);
+        }
+        [HttpGet]
+        [Route("ObtenerTicketsCreadosSinAtender")]
+        public IActionResult ObtenerTicketsCreadosSinAtender(int idAgencia)
+        {
+            string fechaHoy = DateTime.Now.ToString("dd/MM/yyyy");
+            var objTickets = db.Tickets.Where(x => x.IdAgencia == idAgencia && x.Estado == "C" && x.FechaHoraIngreso.Substring(0, 10) == fechaHoy).ToList();
+
+
+            return Ok(objTickets);
+        }
+
+
+    }
+
+    
+}
