01dd997e65611c90cdf364355ef47cebc21fdca6
lacr2591 <66636288+lacr2591@users.noreply.github.com>
2022-08-28 00:08:57 -0500

    camara controller Imagenes
    camara controller Imagenes

---
+++ Controllers/CamarasAgenciasController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bbva_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CamarasAgenciasController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetImagenAgencia")]
+        public IActionResult GetImagenAgencia(int idAgencia)
+        {
+            byte[] imageArray = System.IO.File.ReadAllBytes(@"./Imagenes/"+ idAgencia+".jpg");
+            string base64ImageRepresentation = Convert.ToBase64String(imageArray);
+
+            return Ok(base64ImageRepresentation);
+        }
+
+        
+
+
+    }
+}
